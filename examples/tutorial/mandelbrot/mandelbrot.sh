c9e46f3232710ab604af539960edcebb619a01f5
Ole Weidner <ole.weidner@rutgers.edu>
2013-06-28 11:08:26 -0400

    started 'media' branch

--- examples/tutorial/mandelbrot/mandelbrot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl --insecure -s https://raw.github.com/pypa/virtualenv/master/virtualenv.py | python - /tmp/sagaenv
-. /tmp/sagaenv/bin/activate
-
-pip install PIL
-python mandelbrot.py $@
--- tests/issues/issue_104.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-while true; do 
-  (true &)&
-  echo -n " "
-done 
-
